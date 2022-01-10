0b107872c434b58b898a0bb0b1c63e2f4f19ab24
challah byard <s11169125@students.duvalschools.org>
2022-01-10 09:20:14 -0500

    v0.1

--- Cereal.java
+++ Cereal.java
@@ -1,1 +1,14 @@
-// Challah Byard, 01/10/22, AP Data Lab 2022, v0.0
+// Challah Byard, 01/10/22, AP Data Lab 2022, v0.1
+
+public class Cereal 
+{
+
+    private String cerealName;
+    private double fiber;
+    private double cups;
+    private double fiberPerCup;
+
+
+
+        
+    }
